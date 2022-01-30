52656657d3a5f7394bdd24a8caa02136c7e3592e
Erdi Demir <erdi.demir@icloud.com>
2022-01-30 08:00:33 +0300

    Rapor Konum Bilgisi rabbitmq ya gönderildi

---
+++ Contact.Application/Features/Queries/Contacts/GetKisiIletisimList/GetKisiIletisimListQuery.cs
@@ -0,0 +1,17 @@
+﻿using Contact.Application.Models.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact.Application.Features.Queries.Contacts.GetKisiIletisimList
+{
+    public class GetKisiIletisimListQuery : IRequest<IList<KisiIletisimModel>>
+    {
+        public GetKisiIletisimListQuery()
+        {
+        }
+    }
+}
---
+++ EventBus.Messages/Models/KonumModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Models
+{
+    public  class KonumModel
+    {
+        public string Konum { get; set; }
+        public long KisiSayisi { get; set; }
+        public long TelefonNumarasiSayisi { get; set; }
+    }
+}
